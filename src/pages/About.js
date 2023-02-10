fd52c14b1a22e9eb2e687d58afadd81a3be9c980
nicolas perreau <npnicolas@sfr.fr>
2023-02-10 11:48:27 +0100

    ok

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis vel ducimus corrupti quis quisquam accusantium pariatur minima deleniti eius hic rem perspiciatis, et, quos autem in iusto numquam dolorum expedita distinctio aut? Eaque aperiam cumque, aspernatur suscipit quibusdam nihil adipisci! Consectetur, ipsam deserunt? Nihil quibusdam repellat ex maxime? Pariatur corporis animi quibusdam eligendi aliquam ratione, velit fugit iste obcaecati vitae laborum aperiam dolores, fugiat nihil illo? Maxime sint expedita dolores nisi atque architecto, esse suscipit asperiores, consequatur quam pariatur fuga fugit. Sit, ratione, aliquid ad ipsa cumque excepturi quae nostrum tempora explicabo harum magni pariatur. Accusamus perspiciatis iusto reiciendis eos?</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo praesentium dicta, error commodi asperiores nostrum magnam tempore eos porro? Voluptas facere culpa sequi, maxime quo quam consequuntur praesentium quibusdam hic et inventore eum iusto debitis error voluptate voluptatum itaque autem enim in corporis aliquam recusandae blanditiis repellendus? Nulla voluptas nostrum vero, tenetur deleniti eaque iusto eveniet assumenda reiciendis quod beatae qui tempore consequatur distinctio maxime doloribus quia blanditiis? Ipsum est ipsa perferendis. Eveniet distinctio ipsam facere quam deserunt at quos! Aliquam labore quis sint, vero modi cum magni optio suscipit minus quaerat incidunt placeat repellat libero quisquam. Ab, vero eveniet?</p>
+        </div>
+    );
+};
+
+export default About;
